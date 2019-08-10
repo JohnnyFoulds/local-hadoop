e8dc4307405b7d51984128a81d2c19e1220409cc
Johnny Foulds <hfoulds@gmail.com>
2019-08-10 16:40:07 +0200

    Added PowerCLI Reference Examples

---
+++ PowerCLI/Reference-Examples.ps1
@@ -0,0 +1,18 @@
+﻿param
+(
+[Parameter(Mandatory = $false)] [String]$EsxiHostName = '192.168.3.100',
+[Parameter(Mandatory = $false)] [String]$EsxiUser = 'root',
+[Parameter(Mandatory = $true)] [String]$EsxiPassword
+)
+
+# get a collection of all the virtual machines on the server
+function show-vms
+{
+    $vms = Get-VM -Name *
+    $vms | Format-List
+}
+
+# connect the the exsi server
+$server = Connect-VIServer -Verbose:$true -Server $EsxiHostName -User $EsxiUser -Password $EsxiPassword
+
+show-vms
