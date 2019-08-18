fbfa258fdb50a47c0851fe9eec9c1274e5598adc
Johnny Foulds <hfoulds@gmail.com>
2019-08-18 16:41:50 +0200

    Modified config scripts

--- scripts/config-manager.sh
+++ scripts/config-manager.sh
@@ -1,13 +1,20 @@
 #!/bin/bash 
 
+# set defaults
+USER_NAME=root
+
 # get the command line arguments
-while getopts :u:p:s:i:h: option
+while getopts :u:p:s:i:h:d:g:n: option
 	do
 		 case "${option}" in
 			u) USER_NAME=${OPTARG};;
+			p) PASSWORD=${OPTARG};;
 			s) SERVER=${OPTARG};;
 			i) IP_ADDRESS=${OPTARG};;
 			h) HOST_NAME=${OPTARG};;
+			d) DNS=${OPTARG};;
+			g) GATEWAY=${OPTARG};;
+			n) NETMASK=${OPTARG};;
 		 esac
 	done
 
@@ -17,14 +24,14 @@ echo $PASSWORD
 echo $SERVER
 echo $IP_ADDRESS
 echo $HOST_NAME
+echo $DNS
+echo $GATEWAY
+echo $NETMASK
 echo '---'
 
 echo -e "\e[96mCopying remote config script...\e[39m"
-scp remote-manconfig.sh $USER_NAME@$SERVER:~/
+sshpass -p $PASSWORD scp remote-manconfig.sh $USER_NAME@$SERVER:~/
 
 echo -e "\e[96mExecuting remote config script...\e[39m"
-REMOTE_COMMAND="~/./remote-manconfig.sh -i ${IP_ADDRESS} -h ${HOST_NAME}"
-ssh -t $USER_NAME@$SERVER $REMOTE_COMMAND
-
-#scp config-manager.sh root@192.168.3.161:~/
-#ssh -t root@192.168.3.161 '~/./config-manager.sh'
+REMOTE_COMMAND="~/./remote-manconfig.sh -i ${IP_ADDRESS} -h ${HOST_NAME} -d${DNS} -g${GATEWAY} -n${NETMASK}"
+sshpass -p $PASSWORD ssh -t $USER_NAME@$SERVER $REMOTE_COMMAND
--- scripts/remote-manconfig.sh
+++ scripts/remote-manconfig.sh
@@ -1,22 +1,97 @@
 #!/bin/bash
 
 # get the command line arguments
-while getopts :i:h: option
+while getopts :i:h:d:g:n: option
 	do
 		 case "${option}" in
 			i) IP_ADDRESS=${OPTARG};;
 			h) HOST_NAME=${OPTARG};;
+			d) DNS=${OPTARG};;
+			g) GATEWAY=${OPTARG};;
+			n) NETMASK=${OPTARG};;
 		 esac
 	done
 
 echo '---'
 echo "R: $IP_ADDRESS"
 echo "R: $HOST_NAME"
+echo "R: $DNS"
+echo "R: $GATEWAY"
+echo "R: $NETMASK"
 echo '---'
-
-
 
 # get the network interface to work with
 NETWORK_INTERFACE=$(ip addr | sed -n 's/^2: \(.*\):.*/\1/p')
 
-echo $NETWORK_INTERFACE
+# configure the network settings
+configureNetwork() {
+	# get the network config file
+	NETWORK_CONFIG_PATH="/etc/sysconfig/network-scripts"
+	NETWORK_CONFIG_FILE="$NETWORK_CONFIG_PATH/ifcfg-${NETWORK_INTERFACE}"
+	
+	#make a backup of the network config file
+	#cp $NETWORK_CONFIG_FILE "$NETWORK_CONFIG_FILE.bak"
+	
+	# write the new config file
+	(
+		echo "BOOTPROTO=static" 
+		echo "DEFROUTE=yes"
+		echo "IPV4_FAILURE_FATAL=no"
+		echo "NAME=ens192"
+		echo "DEVICE=ens192"
+		echo "ONBOOT=yes"
+		echo "IPADDR=${IP_ADDRESS}"
+		echo "DNS1=${DNS}"
+		echo "GATEWAY=${GATEWAY}"
+		echo "NETMASK=${NETMASK}"
+	) > $NETWORK_CONFIG_FILE
+	
+	cat $NETWORK_CONFIG_FILE
+}
+
+# disable SELinux
+disableSELinux() {
+	SEL_CONFIG="/etc/selinux/config"
+	
+	sed -i 's/SELINUX=enforcing/SELINUX=disabled/' $SEL_CONFIG
+	
+	cat $SEL_CONFIG
+}
+
+configureHosts() {
+	HOSTS_FILE=/etc/hosts
+	
+	(
+		echo "192.168.3.200	hadoop-manager.lan	hadoop-manager"
+		echo "192.168.3.201	hadoop-worker01.lan	hadoop-worker01"
+		echo "192.168.3.202	hadoop-worker02.lan	hadoop-worker02"
+		echo "192.168.3.203	hadoop-worker03.lan	hadoop-worker03"
+	) > $HOSTS_FILE
+	
+	cat $HOSTS_FILE
+}
+
+# configure the network settings
+echo -e "\e[96mConfiguring Network Settings...\e[39m"
+configureNetwork
+
+# disable the firewall
+echo -e "\e[96mDisabling Firewall...\e[39m"
+systemctl disable firewalld
+systemctl stop firewalld
+
+# disable selinux
+echo -e "\e[96mDisabling SELinux...\e[39m"
+disableSELinux
+
+# set the hostname
+echo -e "\e[96mSetting the hostname...\e[39m"
+hostnamectl set-hostname $HOST_NAME
+
+# configure the dns entries for hosts in the cluster
+echo -e "\e[96mSetting the cluster DNS entries...\e[39m"
+configureHosts
+
+# reboot the machine
+echo -e "\e[96mRebooting the server...\e[39m"
+reboot -h
