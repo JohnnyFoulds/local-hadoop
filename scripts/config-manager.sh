d154fb5564cfa6c423524d74faf31539e02b16fc
Johnny Foulds <hfoulds@gmail.com>
2019-08-18 15:19:36 +0200

    Adde config scripts

--- scripts/config-manager.sh
+++ scripts/config-manager.sh
@@ -5,7 +5,6 @@ while getopts :u:p:s:i:h: option
 	do
 		 case "${option}" in
 			u) USER_NAME=${OPTARG};;
-			p) PASSWORD=${OPTARG};;
 			s) SERVER=${OPTARG};;
 			i) IP_ADDRESS=${OPTARG};;
 			h) HOST_NAME=${OPTARG};;
@@ -20,10 +19,12 @@ echo $IP_ADDRESS
 echo $HOST_NAME
 echo '---'
 
-sshpass -p $PASSWORD scp remote-manconfig.sh $USER_NAME@$SERVER:~/
+echo -e "\e[96mCopying remote config script...\e[39m"
+scp remote-manconfig.sh $USER_NAME@$SERVER:~/
 
+echo -e "\e[96mExecuting remote config script...\e[39m"
 REMOTE_COMMAND="~/./remote-manconfig.sh -i ${IP_ADDRESS} -h ${HOST_NAME}"
-sshpass -p $PASSWORD ssh -t $USER_NAME@$SERVER $REMOTE_COMMAND
+ssh -t $USER_NAME@$SERVER $REMOTE_COMMAND
 
 #scp config-manager.sh root@192.168.3.161:~/
 #ssh -t root@192.168.3.161 '~/./config-manager.sh'
