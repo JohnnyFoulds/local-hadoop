720381260d43e7036ff08afea88cffcdabfe88d3
Johnny Foulds <hfoulds@gmail.com>
2019-08-18 17:08:55 +0200

    Modified manager snapshot script

---
+++ PowerCLI/New-ManagerSnapshot.ps1
@@ -0,0 +1,14 @@
+﻿# server details
+$vCenterHostName = "vcenter-server.lan"
+$vCenterUser = "administrator@vsphere.local"
+
+# instance details
+$datastore = "seagullDatastore"
+$vmHost = "server-pc.lan"
+
+# connect to the vCenter server
+$server = Connect-VIServer -Verbose:$true -Server $vCenterHostName -User $vCenterUser -Password $vCenterPassword
+
+# create a snapshot of the base machine
+$vm = Get-VM -Server $server -Name "Hadoop Manager Node"
+New-Snapshot -Server $server -VM $vm -Name "Manager Base Config" -Description "Hadoop Manager Node Base Config" -Quiesce
