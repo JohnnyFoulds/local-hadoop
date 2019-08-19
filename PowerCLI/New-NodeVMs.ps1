5c0d4af9a99febe737b49c15f4212b5939142071
Johnny Foulds <hfoulds@gmail.com>
2019-08-19 19:12:28 +0200

    Scripts for worker nod creation

---
+++ PowerCLI/New-NodeVMs.ps1
@@ -0,0 +1,38 @@
+﻿# server details
+$vCenterHostName = "vcenter-server.lan"
+$vCenterUser = "administrator@vsphere.local"
+
+# instance details
+$datastore = "seagullDatastore"
+$vmHost = "server-pc.lan"
+
+function New-WorkerNode([string]$name) {
+    $cloneParams = @{
+        'Name' = $name
+        'Datastore' = $datastore 
+        'VM' = $vm
+        'DiskStorageFormat' = 'Thin'
+        'VMHost' = $vmHost
+        'LinkedClone' = $null
+        'ReferenceSnapshot' = $snapshot
+    }
+
+    # clone the vm
+    $workerVM = New-VM @cloneParams
+
+    # add cpu cores and memory
+    Set-VM -VM $workerVM -NumCpu 2 -MemoryGB 2 -Confirm:$false
+}
+
+# connect to the vCenter server
+$server = Connect-VIServer -Verbose:$true -Server $vCenterHostName -User $vCenterUser -Password $vCenterPassword
+
+# get the manager node and snapshot to create the clones form
+$vm = Get-VM -Server $server -Name "Hadoop Manager Node"
+$snapshot = Get-Snapshot -VM $vm -Name "Manager Base Config"
+
+# create the three worker nodes
+New-WorkerNode -name "Hadoop Worker Node 01"
+New-WorkerNode -name "Hadoop Worker Node 02"
+New-WorkerNode -name "Hadoop Worker Node 03"
+
---
+++ PowerCLI/Shutdown-VMs.ps1
@@ -0,0 +1,8 @@
+﻿# server details
+$vCenterHostName = "vcenter-server.lan"
+$vCenterUser = "administrator@vsphere.local"
+
+Get-VM -Server $server -Name "Hadoop Manager Node"   | Shutdown-VMGuest -Confirm:$false
+Get-VM -Server $server -Name "Hadoop Worker Node 01" | Shutdown-VMGuest -Confirm:$false
+Get-VM -Server $server -Name "Hadoop Worker Node 02" | Shutdown-VMGuest -Confirm:$false
+Get-VM -Server $server -Name "Hadoop Worker Node 03" | Shutdown-VMGuest -Confirm:$false
