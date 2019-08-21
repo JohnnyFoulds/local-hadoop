5155d08ac67da7647e902b263ddd3920648c7c02
Johnny Foulds <hfoulds@gmail.com>
2019-08-21 20:23:29 +0200

    Added script to start all servers in the cluster.

---
+++ PowerCLI/Start-Cluster.ps1
@@ -0,0 +1,11 @@
+﻿# server details
+$vCenterHostName = "vcenter-server.lan"
+$vCenterUser = "administrator@vsphere.local"
+
+# connect to the vCenter server
+$server = Connect-VIServer -Verbose:$true -Server $vCenterHostName -User $vCenterUser -Password $vCenterPassword
+
+Get-VM -Server $server -Name "Hadoop Manager Node"   | Start-VM -Confirm:$false
+Get-VM -Server $server -Name "Hadoop Worker Node 01" | Start-VM -Confirm:$false
+Get-VM -Server $server -Name "Hadoop Worker Node 02" | Start-VM -Confirm:$false
+Get-VM -Server $server -Name "Hadoop Worker Node 03" | Start-VM -Confirm:$false
